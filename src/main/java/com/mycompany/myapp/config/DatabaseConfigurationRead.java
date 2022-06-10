fc733b50ffbd904fa86c6014ec0cb03dc98268ea
Duy Huynh <duyhuynh61@gmail.com>
2022-06-10 14:07:58 +0700

    init

---
+++ src/main/java/com/mycompany/myapp/config/DatabaseConfigurationRead.java
@@ -0,0 +1,127 @@
+package com.mycompany.myapp.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateProperties;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateSettings;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.hibernate5.SpringBeanContainer;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.util.ClassUtils;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableJpaRepositories(
+    entityManagerFactoryRef = "readEntityManagerFactory",
+    transactionManagerRef = "readTransactionManager",
+    basePackages = {DatabaseConfigurationRead.REPOSITORY_PACKAGE_TO_SCAN}
+)
+@EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
+@EnableTransactionManagement
+public class DatabaseConfigurationRead {
+
+    public static final String PERSISTENCE_UNIT_NAME = "read";
+    public static final String REPOSITORY_PACKAGE_TO_SCAN = "com.mycompany.myapp.repository.read";
+    public static final String MODEL_PACKAGE_TO_SCAN = "com.mycompany.myapp.domain";
+
+    private final JpaProperties jpaProperties;
+    private final HibernateProperties hibernateProperties;
+    private final List<HibernatePropertiesCustomizer> hibernatePropertiesCustomizers;
+
+    public DatabaseConfigurationRead(JpaProperties jpaProperties, HibernateProperties hibernateProperties,
+                                     ConfigurableListableBeanFactory beanFactory,
+                                     ObjectProvider<HibernatePropertiesCustomizer> hibernatePropertiesCustomizers) {
+        this.jpaProperties = jpaProperties;
+        this.hibernateProperties = hibernateProperties;
+        this.hibernatePropertiesCustomizers = determineHibernatePropertiesCustomizers(beanFactory,
+            hibernatePropertiesCustomizers.orderedStream().collect(Collectors.toList()));
+    }
+
+    @Bean(name = "readDataSourceProperties")
+    @ConfigurationProperties("spring.read-datasource")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean(name = "readDataSource")
+    @ConfigurationProperties(prefix = "spring.read-datasource.hikari")
+    public DataSource dataSource(@Qualifier("readDataSourceProperties") DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+    }
+
+    @Bean(name = "readEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(EntityManagerFactoryBuilder builder,
+                                                                       @Qualifier("readDataSource") DataSource dataSource) {
+        final Map<String, Object> vendorProperties = this.getVendorProperties();
+        this.customizeVendorProperties(vendorProperties);
+
+        return builder
+            .dataSource(dataSource)
+            .persistenceUnit(PERSISTENCE_UNIT_NAME)
+            .packages(MODEL_PACKAGE_TO_SCAN)
+            .properties(vendorProperties)
+            .build();
+    }
+
+    @Bean(name = "readTransactionManager")
+    public PlatformTransactionManager transactionManager(
+        @Qualifier("readEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+
+    private List<HibernatePropertiesCustomizer> determineHibernatePropertiesCustomizers(
+        ConfigurableListableBeanFactory beanFactory,
+        List<HibernatePropertiesCustomizer> hibernatePropertiesCustomizers) {
+
+        List<HibernatePropertiesCustomizer> customizers = new ArrayList();
+        if (ClassUtils.isPresent("org.hibernate.resource.beans.container.spi.BeanContainer",
+            this.getClass().getClassLoader())) {
+            customizers.add((properties) -> {
+                properties.put("hibernate.resource.beans.container", new SpringBeanContainer(beanFactory));
+            });
+        }
+
+        customizers.addAll(hibernatePropertiesCustomizers);
+        return customizers;
+    }
+
+    private Map<String, Object> getVendorProperties() {
+        return new LinkedHashMap<>(
+            this.hibernateProperties.determineHibernateProperties(this.jpaProperties.getProperties(),
+                new HibernateSettings()
+                    // Spring Boot's HibernateDefaultDdlAutoProvider is not available here
+                    .hibernatePropertiesCustomizers(this.hibernatePropertiesCustomizers)
+            )
+        );
+    }
+
+    protected void customizeVendorProperties(Map<String, Object> vendorProperties) {
+        if (!vendorProperties.containsKey("hibernate.transaction.jta.platform")) {
+            //
+        }
+
+        if (!vendorProperties.containsKey("hibernate.connection.provider_disables_autocommit")) {
+            //
+        }
+    }
+}
---
+++ src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
@@ -0,0 +1,80 @@
+package com.mycompany.myapp.config;
+
+import com.mycompany.myapp.security.*;
+import com.mycompany.myapp.security.jwt.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
+import tech.jhipster.config.JHipsterProperties;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@Import(SecurityProblemSupport.class)
+public class SecurityConfiguration {
+
+    private final JHipsterProperties jHipsterProperties;
+
+    private final TokenProvider tokenProvider;
+    private final SecurityProblemSupport problemSupport;
+
+    public SecurityConfiguration(
+        TokenProvider tokenProvider,
+        JHipsterProperties jHipsterProperties,
+        SecurityProblemSupport problemSupport
+    ) {
+        this.tokenProvider = tokenProvider;
+        this.problemSupport = problemSupport;
+        this.jHipsterProperties = jHipsterProperties;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+            .csrf()
+            .disable()
+            .exceptionHandling()
+                .authenticationEntryPoint(problemSupport)
+                .accessDeniedHandler(problemSupport)
+        .and()
+            .headers()
+            .contentSecurityPolicy(jHipsterProperties.getSecurity().getContentSecurityPolicy())
+        .and()
+            .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN)
+        .and()
+            .permissionsPolicy().policy("camera=(), fullscreen=(self), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), midi=(), payment=(), sync-xhr=()")
+        .and()
+            .frameOptions()
+            .deny()
+        .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+            .authorizeRequests()
+            .antMatchers("/api/authenticate").permitAll()
+            .antMatchers("/api/admin/**").hasAuthority(AuthoritiesConstants.ADMIN)
+            .antMatchers("/api/**").authenticated()
+            .antMatchers("/management/health").permitAll()
+            .antMatchers("/management/health/**").permitAll()
+            .antMatchers("/management/info").permitAll()
+            .antMatchers("/management/prometheus").permitAll()
+            .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
+        .and()
+            .apply(securityConfigurerAdapter());
+        return http.build();
+        // @formatter:on
+    }
+
+    private JWTConfigurer securityConfigurerAdapter() {
+        return new JWTConfigurer(tokenProvider);
+    }
+}
